c1bec65d92eb3ef47f05fa60e8ba7f62c1b25ce2
Sam Gehman <sgehman@cs.uw.edu>
2019-03-22 18:53:04 -0700

    Update quarter.js

--- js/quarter.js
+++ js/quarter.js
@@ -4,15 +4,17 @@
  */
 
 
-var quarter = function() {
-    return "Autumn 2017";
-}
+var quarter = function () {
+    return "Spring 2019";
+};
 
-var qtr = function() {
-    return "17au";
-}
+var qtr = function () {
+    return "19au";
+};
 
-Course.name = "CSE 351";
+Course.name = "CSE 180";
+
+// TODO: find out the Canvas code
 Course.canvas = "1115906";
 
 Course.setSchedule("09/27/17", "MWF", "R", 11);
@@ -31,28 +33,31 @@ Course.Lectures = {
     B: {from: "2:30pm", to: "3:20pm", location: "EEB 125"}
 }
 
+// TODO: update this with real information
 Course.Staff = [
-    {netid: "jhsia",    name: "Justin Hsia"},
-    {netid: "wottol",   name: "Lucas Wotton"}, 
+    {netid: "jhsia", name: "Justin Hsia"},
+    {netid: "wottol", name: "Lucas Wotton"},
     {netid: "mjqzhang", name: "Michael Zhang"},
     {netid: "pdewilde", name: "Parker DeWilde"},
-    {netid: "ryanww",   name: "Ryan Wong"},
-    {netid: "sgehman",  name: "Sam Gehman"},
-    {netid: "wolfson",  name: "Sam Wolfson"},
-    {netid: "savannay", name: "Savanna Yee"}, 
-    {netid: "vinnyp",   name: "Vinny Palaniappan"}
-]
+    {netid: "ryanww", name: "Ryan Wong"},
+    {netid: "sgehman", name: "Sam Gehman"},
+    {netid: "wolfson", name: "Sam Wolfson"},
+    {netid: "savannay", name: "Savanna Yee"},
+    {netid: "vinnyp", name: "Vinny Palaniappan"}
+];
 
+// TODO: update this with real information
 Course.Sections = {
-    AA: {time: "8:30am",  location: "EEB 025", ta: "wottol"},
-    AB: {time: "9:30am",  location: "EEB 037", ta: "wolfson"},
+    AA: {time: "8:30am", location: "EEB 025", ta: "wottol"},
+    AB: {time: "9:30am", location: "EEB 037", ta: "wolfson"},
     AC: {time: "11:30am", location: "EEB 025", ta: ["sgehman", "pdewilde"]},
     AD: {time: "12:30pm", location: "EEB 031", ta: "savannay"},
     BA: {time: "12:30pm", location: "SIG 225", ta: "mjqzhang"},
-    BB: {time: "1:30pm",  location: "CHL 015", ta: "ryanww"},
-    BC: {time: "2:30pm",  location: "SAV 166", ta: "vinnyp"}
-}
+    BB: {time: "1:30pm", location: "CHL 015", ta: "ryanww"},
+    BC: {time: "2:30pm", location: "SAV 166", ta: "vinnyp"}
+};
 
+// TODO: update this with real information
 Course.OfficeHours = {
     DefaultLocation: "2nd Floor Breakout",
     Times: {
@@ -60,7 +65,7 @@ Course.OfficeHours = {
             {from: "9:30am", to: "10:30am", who: "savannay", exceptions: ["12/11/17"]},
             {from: "12:30pm", to: "1:30pm", who: "wolfson", exceptions: ["12/11/17"]},
             {from: "3:30pm", to: "4:30pm", who: "vinnyp", exceptions: ["12/11/17"]},
-            {from: "5:00pm", to: "6:00pm", who: "jhsia", location: "CSE 438", exceptions: ["10/30/17","12/11/17"]}
+            {from: "5:00pm", to: "6:00pm", who: "jhsia", location: "CSE 438", exceptions: ["10/30/17", "12/11/17"]}
         ],
         tuesday: [
             {from: "10:30am", to: "11:30am", who: "ryanww", exceptions: ["11/7/17"]},
@@ -68,7 +73,7 @@ Course.OfficeHours = {
             {from: "4:30pm", to: "5:30pm", who: "pdewilde", exceptions: ["12/12/17"]}
         ],
         wednesday: [
-            {from: "9:30am", to: "10:30am", who: "sgehman", exceptions: ["9/27/17","11/15/17"]},
+            {from: "9:30am", to: "10:30am", who: "sgehman", exceptions: ["9/27/17", "11/15/17"]},
             {from: "12:30pm", to: "1:30pm", who: "vinnyp", exceptions: ["11/15/17"]},
             {from: "4:30pm", to: "5:30pm", who: "jhsia", location: "CSE 438", exceptions: []}
         ],
@@ -78,31 +83,77 @@ Course.OfficeHours = {
             {from: "6:30pm", to: "7:30pm", who: "pdewilde", exceptions: ["12/14/17"]}
         ],
         friday: [
-            {from: "9:30am", to: "10:30am", who: "savannay", exceptions: ["11/10/17","11/24/17"]},
-            {from: "10:30am", to: "11:30am", who: "ryanww", exceptions: ["11/10/17","11/24/17"]},
-            {from: "12:30pm", to: "1:30pm", who: "wottol", exceptions: ["11/10/17","11/24/17"]},
-            {from: "3:30pm", to: "4:30pm", who: "jhsia", location: "CSE 438", exceptions: ["11/3/17","11/10/17","11/24/17"]},
-            {from: "4:30pm", to: "5:30pm", who: "wottol", exceptions: ["11/10/17","11/24/17"]}
+            {from: "9:30am", to: "10:30am", who: "savannay", exceptions: ["11/10/17", "11/24/17"]},
+            {from: "10:30am", to: "11:30am", who: "ryanww", exceptions: ["11/10/17", "11/24/17"]},
+            {from: "12:30pm", to: "1:30pm", who: "wottol", exceptions: ["11/10/17", "11/24/17"]},
+            {
+                from: "3:30pm",
+                to: "4:30pm",
+                who: "jhsia",
+                location: "CSE 438",
+                exceptions: ["11/3/17", "11/10/17", "11/24/17"]
+            },
+            {from: "4:30pm", to: "5:30pm", who: "wottol", exceptions: ["11/10/17", "11/24/17"]}
         ],
         saturday: [],
         sunday: []
     }
-}
+};
 
+// TODO: update this with real information
 Course.Events = [
     {name: "Midterm Review", location: "EEB 105", start: "10/27/17 5:30pm", end: "10/27/17 7:30pm", exceptions: []},
     {name: "Final Review", location: "EEB 105", start: "12/11/17 5:00pm", end: "12/11/17 8:00pm", exceptions: []},
-    {name: "Midterm Office Hours", location: "CSE 438", start: "10/29/17 6:30pm", end: "10/29/17 8:30pm", exceptions: []},
-    {name: "Midterm Office Hours", location: "CSE 438", start: "10/30/17 10:30am", end: "10/30/17 12:30pm", exceptions: []},
-    {name: "Midterm Office Hours", location: "CSE 438", start: "10/30/17 2:30pm", end: "10/30/17 3:30pm", exceptions: []},
+    {
+        name: "Midterm Office Hours",
+        location: "CSE 438",
+        start: "10/29/17 6:30pm",
+        end: "10/29/17 8:30pm",
+        exceptions: []
+    },
+    {
+        name: "Midterm Office Hours",
+        location: "CSE 438",
+        start: "10/30/17 10:30am",
+        end: "10/30/17 12:30pm",
+        exceptions: []
+    },
+    {
+        name: "Midterm Office Hours",
+        location: "CSE 438",
+        start: "10/30/17 2:30pm",
+        end: "10/30/17 3:30pm",
+        exceptions: []
+    },
     {name: "Special Office Hours", location: "CSE 438", start: "11/3/17 1:30pm", end: "11/3/17 2:20pm", exceptions: []},
-    {name: "Special Office Hours", location: "2nd Floor Breakout", start: "11/9/17 4:30pm", end: "11/9/17 5:30pm", exceptions: []},
-    {name: "Special Office Hours", location: "CSE 007", start: "11/15/17 9:30am", end: "11/15/17 10:30am", exceptions: []},
-    {name: "Special Office Hours", location: "3rd Floor Breakout", start: "11/15/17 12:30pm", end: "11/15/17 1:30pm", exceptions: []},
-    {name: "Final Office Hours", location: "CSE 438", start: "12/11/17 10:30am", end: "12/11/17 11:30am", exceptions: []},
+    {
+        name: "Special Office Hours",
+        location: "2nd Floor Breakout",
+        start: "11/9/17 4:30pm",
+        end: "11/9/17 5:30pm",
+        exceptions: []
+    },
+    {
+        name: "Special Office Hours",
+        location: "CSE 007",
+        start: "11/15/17 9:30am",
+        end: "11/15/17 10:30am",
+        exceptions: []
+    },
+    {
+        name: "Special Office Hours",
+        location: "3rd Floor Breakout",
+        start: "11/15/17 12:30pm",
+        end: "11/15/17 1:30pm",
+        exceptions: []
+    },
+    {
+        name: "Final Office Hours",
+        location: "CSE 438",
+        start: "12/11/17 10:30am",
+        end: "12/11/17 11:30am",
+        exceptions: []
+    },
     {name: "Final Office Hours", location: "CSE 438", start: "12/11/17 3:30pm", end: "12/11/17 4:30pm", exceptions: []},
     {name: "Final Office Hours", location: "CSE 438", start: "12/12/17 12:30pm", end: "12/12/17 4:30pm", exceptions: []}
-    //{name: "Workshop", location: "OUG 136", start: "10/27/17 6:00pm", end: "10/27/17 8:00pm", recurring: {type: "weekly", day: "Wednesday"}, exceptions: ["05/03/17"]}
-]
-
-//Course.Exams.addExam("mt-rev", "Midterm Review", "10/27/17", "6:00pm", "8:00pm", "TBD");
+];
